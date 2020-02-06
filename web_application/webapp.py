db884a8c3c6f22185b6087e4a2c1e5820dc9b9c6
maiar <maiar@outlook.com>
2020-02-06 13:28:20 -0500

    Initial design of the web app
    Add a table and two calendar selectors

--- web_application/webapp.py
+++ web_application/webapp.py
@@ -1,31 +1,53 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+from datetime import datetime as dt
+import boto3
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+app.layout = html.Div([
+    dcc.DatePickerRange(
+        id='my-date-picker-range',
+        min_date_allowed=dt(1995, 8, 5),
+        max_date_allowed=dt(2017, 9, 19),
+        initial_visible_month=dt(2017, 8, 5),
+        end_date=dt(2017, 8, 25)
+    ),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    html.Label('Slider'),
+    dcc.Slider(
+        min=0,
+        max=9,
+        marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
+        value=5,
+    ),
+    
+    html.Div([
+        html.H4(children='News Leaders: trace the most cited news articles'),
+        generate_table(df)
+    ])
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
+    
+
+], style={'columnCount': 1})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+    #app.run_server(debug=True, port = 8080, host='ec2-35-171-44-44.compute-1.amazonaws.com')
