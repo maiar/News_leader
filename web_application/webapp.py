8fbe56abc7815e6b0d8018bfbe7069272a01c2e9
Ubuntu <ubuntu@ip-10-0-0-4.ec2.internal>
2020-02-07 20:16:19 +0000

    Update the frontend

--- web_application/webapp.py
+++ web_application/webapp.py
@@ -3,9 +3,23 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 from datetime import datetime as dt
-import boto3
+import psycopg2
+import psycopg2.extras
 
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+conn = psycopg2.connect(
+    database="newsLeader",
+    user="postgres",
+    password="Tianya1990",
+    host="liangchun-database.csbeke3v1jfz.us-east-1.rds.amazonaws.com",
+    port='5432'
+)
+
+cur = conn.cursor()
+
+newsDf = cur.execute("""SELECT "MentionIdentifier", "NumofCitation" FROM public.news ORDER BY "NumofCitation" DESC LIMIT 10""")
+rows = cur.fetchall()
+
+df = pd.DataFrame(rows)
 
 def generate_table(dataframe, max_rows=10):
     return html.Table(
@@ -23,31 +37,24 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
+    html.Div([
+        html.H1(children='News Leaders: trace the most cited news articles')
+    ]),
+
     dcc.DatePickerRange(
         id='my-date-picker-range',
         min_date_allowed=dt(1995, 8, 5),
-        max_date_allowed=dt(2017, 9, 19),
-        initial_visible_month=dt(2017, 8, 5),
-        end_date=dt(2017, 8, 25)
-    ),
-
-    html.Label('Slider'),
-    dcc.Slider(
-        min=0,
-        max=9,
-        marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
-        value=5,
+        max_date_allowed=dt(2020, 2, 7),
+        initial_visible_month=dt(2020, 2, 6),
+        end_date=dt(2019, 12, 25),
+        start_date = dt(2019, 11, 25)
     ),
     
     html.Div([
-        html.H4(children='News Leaders: trace the most cited news articles'),
+        html.H4(children='Table view'),
         generate_table(df)
-    ])
-
-    
-
-], style={'columnCount': 1})
+    ], style={'columnCount': 1,'width': '52%'})
+])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    #app.run_server(debug=True, port = 8080, host='ec2-35-171-44-44.compute-1.amazonaws.com')
+    app.run_server(debug=True, port = 8060, host='ec2-35-171-44-44.compute-1.amazonaws.com')
