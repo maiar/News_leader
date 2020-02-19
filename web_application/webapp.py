a230a0f8b965a40692dae09f86e4a72841c67f73
maiar <maiar@outlook.com>
2020-02-19 10:48:38 -0500

    Update database connection
    add read_sql function

--- web_application/webapp.py
+++ web_application/webapp.py
@@ -5,7 +5,10 @@ import pandas as pd
 from datetime import datetime as dt
 import psycopg2
 import psycopg2.extras
+import pandas.io.sql as psql
 import plotly.graph_objs as go
+import dash_table
+from dash.dependencies import Input, Output, State
 
 conn = psycopg2.connect(
     database="newsLeader",
@@ -15,13 +18,16 @@ conn = psycopg2.connect(
     port='5432'
 )
 
-cur = conn.cursor()
+df = psql.read_sql("""SELECT a.*, b.count FROM public.events a INNER JOIN (SELECT * FROM public.citations AS ranks ORDER BY "count" DESC LIMIT 10) b ON a."GlobalEventID" = b."GlobalEventID" ORDER BY "count" DESC;""", conn)
+df['EventDateTime'] = pd.to_datetime(df['EventDateTime'], format='%Y%m%d', errors='coerce')
+df.EventDateTime = pd.DatetimeIndex(df.EventDateTime).strftime("%Y-%m-%d")
+#df.index = df['EventDateTime']
 
-newsDf = cur.execute("""SELECT a.*, b.count FROM public.events a INNER JOIN (SELECT * FROM public.citations AS ranks ORDER BY "count" DESC LIMIT 10) b ON a."GlobalEventID" = b."GlobalEventID" ORDER BY "count" DESC;""")
-rows = cur.fetchall()
-print(rows.columns)
+conn = None
 
-df = pd.DataFrame(rows)
+print(df.columns)
+print(df.dtypes)
+print("\n")
 
 def generate_table(dataframe, max_rows=10):
     return html.Table(
@@ -42,7 +48,7 @@ app.layout = html.Div([
     html.Div([
             html.Img(
             src=app.get_asset_url('frontend.png'),
-            style={'width': '100%', 'margin': '0'}
+            style={'width': '100%'}
         ),
     ]),
 
@@ -54,11 +60,11 @@ app.layout = html.Div([
         html.Div([
             dcc.DatePickerRange(
                 id='date-picker-range',
-                min_date_allowed=dt(1995, 8, 5),
-                max_date_allowed=dt(2020, 2, 7),
-                initial_visible_month=dt(2020, 2, 6),
-                end_date=dt(2019, 12, 25),
-                start_date = dt(2019, 11, 25),
+                min_date_allowed=df['EventDateTime'].min(),
+                max_date_allowed=df['EventDateTime'].max(),
+                initial_visible_month=df['EventDateTime'].min(),
+                start_date=df['EventDateTime'].min(),
+                end_date=df['EventDateTime'].max(),
                 style={'margin-right': '1.5em'}
             ), 
             html.Button('Submit', id='button')
@@ -72,8 +78,8 @@ app.layout = html.Div([
                 figure={
                     "data": [
                         go.Bar(
-                            x=list(str(df[0])),
-                            y=df[1],
+                            x=df['Url'],
+                            y=df['count'],
                             marker={
                                 "color": "#97151c",
                                 "line": {
@@ -87,26 +93,56 @@ app.layout = html.Div([
                 },
             ),
             html.H4(children='Table view'),
-            generate_table(df)        
+            
+            #html.Div(id='table')
+            generate_table(df)
         ]),
     ], style={'width': '80%','margin': '0 auto', 'text-align': 'center'})   
-], style={'margin': '0 auto'}
+], style={'margin': '0'}
 )
-
 '''
-@app.callback(
-    dash.dependencies.Output('bar-plot', 'selectedData'),
-    [dash.dependencies.Input('button', 'n_clicks')],
-    [dash.dependencies.State('date-picker-range', 'start_date')])
-def update_output(n_clicks, start_date):
-    string_prefix = 'You have selected: '
-    if date is not None:
-        date = dt.strptime(date.split(' ')[0], '%Y-%m-%d')
-        date_string = date.strftime('%B %d, %Y')
-        return string_prefix + date_string
-    print(string_prefix+date_string)
+@app.callback(Output('table', 'children'),
+			 [Input('button', 'n_clicks')],
+			 [State('date-picker-range', 'start_date'), State('date-picker-range', 'end_date')])
+def update_graph(n_clicks, start_date, end_date):
+    startDate = int(start_date[:10].replace("-",""))
+    endDate = int(end_date[:10].replace("-","")) 
+    print(startDate)
+    print(endDate)
+    print(type(start_date))
+    print(type(end_date))
+    start = dt.strptime(start_date, "%Y-%m-%d")
+    end = dt.strptime(end_date, '%Y-%m-%d')
+    print(start.strftime("%Y%m%d"))
+    print(end.strftime("%Y%m%d"))
+
+    conn = psycopg2.connect(
+        database="newsLeader",
+        user="postgres",
+        password="Tianya1990",
+        host="liangchun-database.csbeke3v1jfz.us-east-1.rds.amazonaws.com",
+        port='5432'
+    )
+
+    df = psql.read_sql("""SELECT a.*, b.count FROM public.events a INNER JOIN (SELECT * FROM public.citations AS ranks ORDER BY "count" DESC LIMIT 10) b ON a."GlobalEventID" = b."GlobalEventID" ORDER BY "count" DESC;""", conn)
+    df['EventDateTime'] = pd.to_datetime(df['EventDateTime'], format='%Y%m%d', errors='coerce')
+    #df.EventDateTime = pd.DatetimeIndex(df.EventDateTime).strftime("%Y-%m-%d")
+    #df.index = df['EventDateTime']
+
+    conn = None
+
+    print(df.columns)
+    print(df.dtypes)
+    print("\n")
+    #df2 = df.loc[start: end]
+    #print(type(df2))
+
+    df.EventDateTime = pd.DatetimeIndex(df.EventDateTime).strftime("%Y-%m-%d")
+    # df2.EventDateTime = pd.DatetimeIndex(df2.EventDateTime).strftime("%Y-%m-%d")
+
+    return generate_table(df, max_rows=10)
 '''
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port = 8060, host='ec2-35-171-44-44.compute-1.amazonaws.com')
-    #app.run_server(debug=True)
+    #app.run_server(debug=True, port = 8060, host='ec2-35-171-44-44.compute-1.amazonaws.com')
+    app.run_server(debug=True)
