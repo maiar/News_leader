ca9d2c7d9f1b4dc3ef81f3a3437bd85ead07b247
Ubuntu <ubuntu@ip-10-0-0-4.ec2.internal>
2020-02-20 03:04:21 +0000

    Add newsbase table

--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,1 @@
-#~/.local/bin/spark-submit --master spark://ec2-35-171-44-44.compute-1.amazonaws.com:7077 datapipeline.py
-~/.local/bin/spark-submit --master spark://ec2-35-171-44-44.compute-1.amazonaws.com:7077 dpline.py
+~/.local/bin/spark-submit --master spark://ec2-35-171-44-44.compute-1.amazonaws.com:7077 datapipeline.py
