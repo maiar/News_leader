02c24a029f93a7388f57cb6e222a219ddaadfb6d
maiar <maiar@outlook.com>
2020-02-21 13:52:01 -0500

    Add the map
    add the map

--- web_application/webapp.py
+++ web_application/webapp.py
@@ -9,6 +9,8 @@ import pandas.io.sql as psql
 import plotly.graph_objs as go
 import dash_table
 from dash.dependencies import Input, Output, State
+import plotly.express as px
+import plotly.graph_objects as go
 
 conn = psycopg2.connect(
     database="newsLeader",
@@ -18,7 +20,9 @@ conn = psycopg2.connect(
     port='5432'
 )
 
-df = psql.read_sql("""SELECT * FROM public.newshouse WHERE "EventDateTime" BETWEEN 20190118 AND 20190125 ORDER BY "ArticleCounts" DESC LIMIT 10;""", conn)
+df = "global"
+
+df = psql.read_sql("""SELECT * FROM public.news2020 WHERE "EventDateTime" BETWEEN 20191219 AND 20200125 ORDER BY "ArticleCounts" DESC LIMIT 10;""", conn)
 # df = psql.read_sql("""SELECT * FROM public.newsbase WHERE "EventDateTime" BETWEEN 20191219 AND 20200125 ORDER BY "ArticleCounts" DESC LIMIT 10;""", conn)
 df['EventDateTime'] = pd.to_datetime(df['EventDateTime'], format='%Y%m%d', errors='coerce')
 df.EventDateTime = pd.DatetimeIndex(df.EventDateTime).strftime("%Y-%m-%d")
@@ -26,9 +30,11 @@ df.EventDateTime = pd.DatetimeIndex(df.EventDateTime).strftime("%Y-%m-%d")
 
 conn = None
 
-print(df.columns)
-print(df.dtypes)
-print("\n")
+def generate_map(dataframe, max_rows=10):
+    px.set_mapbox_access_token("pk.eyJ1IjoibHh1MDEiLCJhIjoiY2s2dHp5Z3F4MDRzYTNocG5hbzNvdXZzMiJ9.xceyt17S85YHqeAe9jxVzA")
+    return px.scatter_mapbox(df, lat="Latitude", lon="Longitude", \
+        color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, \
+        zoom=1)
 
 def generate_table(dataframe, max_rows=10):
     return html.Table(
@@ -40,6 +46,26 @@ def generate_table(dataframe, max_rows=10):
             html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
         ]) for i in range(min(len(dataframe), max_rows))]
     )
+
+def generate_plot(dataframe, max_rows=10):
+    return dcc.Graph(
+            figure={
+                "data": [
+                    go.Bar(
+                        x=list(range(1,11)),
+                        y=dataframe['ArticleCounts'],
+                        marker={
+                            "color": "#97151c",
+                            "line": {
+                                "color": "rgb(255, 255, 255)",
+                                "width": 2,
+                            },
+                        },
+                        name="News leaders",
+                    ),
+                ], 
+            },
+        )
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -70,54 +96,38 @@ app.layout = html.Div([
             ), 
             html.Button('Submit', id='button')
         ]),
-    
-    
+
         html.Div([
             html.H4(children='Plot view'),
-            dcc.Graph(
-                id="bar-plot",
-                figure={
-                    "data": [
-                        go.Bar(
-                            x=df['SourceUrl'],
-                            y=df['ArticleCounts'],
-                            marker={
-                                "color": "#97151c",
-                                "line": {
-                                    "color": "rgb(255, 255, 255)",
-                                    "width": 2,
-                                },
-                            },
-                            name="Calibre Index Fund",
-                        ),
-                    ], 
-                },
-            ),
+
+            html.Div(id="bar-plot"),
+
+            html.H4(children='Map view'),
+
+            dcc.Graph(id='map'),
+
+            # html.Div(id="map"),
+
             html.H4(children='Table view'),
             
-            #html.Div(id='table')
-            generate_table(df),
+            html.Div(id='table'),
 
-            html.H3(children='Liangchun Xu\t\t\tTufts PhD | Insight Data Engineering')
+            html.H3(children='Liangchun Xu\tTufts PhD | Insight Data Science')
         ]),
     ], style={'width': '80%','margin': '0 auto', 'text-align': 'center'})   
 ], style={'margin': '0'}
 )
-'''
-@app.callback(Output('table', 'children'),
+
+@app.callback([Output('table', 'children'), Output('bar-plot', 'children'),  Output('map', 'figure')],
 			 [Input('button', 'n_clicks')],
 			 [State('date-picker-range', 'start_date'), State('date-picker-range', 'end_date')])
-def update_graph(n_clicks, start_date, end_date):
+def update_table(n_clicks, start_date, end_date):
     startDate = int(start_date[:10].replace("-",""))
     endDate = int(end_date[:10].replace("-","")) 
-    print(startDate)
-    print(endDate)
-    print(type(start_date))
-    print(type(end_date))
     start = dt.strptime(start_date, "%Y-%m-%d")
     end = dt.strptime(end_date, '%Y-%m-%d')
-    print(start.strftime("%Y%m%d"))
-    print(end.strftime("%Y%m%d"))
+    start = int(start.strftime("%Y%m%d"))
+    end = int(end.strftime("%Y%m%d"))
 
     conn = psycopg2.connect(
         database="newsLeader",
@@ -127,25 +137,14 @@ def update_graph(n_clicks, start_date, end_date):
         port='5432'
     )
 
-    df = psql.read_sql("""SELECT a.*, b.count FROM public.events a INNER JOIN (SELECT * FROM public.citations AS ranks ORDER BY "count" DESC LIMIT 10) b ON a."GlobalEventID" = b."GlobalEventID" ORDER BY "count" DESC;""", conn)
+    df = psql.read_sql("""SELECT * FROM public.news2020 WHERE "EventDateTime" BETWEEN %(dstart)s AND %(dfinish)s ORDER BY "ArticleCounts" DESC LIMIT 10;""", conn, params={"dstart":int(start),"dfinish":int(end)})
     df['EventDateTime'] = pd.to_datetime(df['EventDateTime'], format='%Y%m%d', errors='coerce')
-    #df.EventDateTime = pd.DatetimeIndex(df.EventDateTime).strftime("%Y-%m-%d")
-    #df.index = df['EventDateTime']
 
     conn = None
 
-    print(df.columns)
-    print(df.dtypes)
-    print("\n")
-    #df2 = df.loc[start: end]
-    #print(type(df2))
-
     df.EventDateTime = pd.DatetimeIndex(df.EventDateTime).strftime("%Y-%m-%d")
-    # df2.EventDateTime = pd.DatetimeIndex(df2.EventDateTime).strftime("%Y-%m-%d")
-
-    return generate_table(df, max_rows=10)
-'''
+    return generate_table(df, max_rows=10), generate_plot(df, max_rows=10),  generate_map(df, max_rows=10) 
 
 if __name__ == '__main__':
-    #app.run_server(debug=True, port = 8060, host='ec2-35-171-44-44.compute-1.amazonaws.com')
-    app.run_server(debug=True)
+    app.run_server(debug=True, port = 8080, host='ec2-35-171-44-44.compute-1.amazonaws.com')
+    #app.run_server(debug=True)
